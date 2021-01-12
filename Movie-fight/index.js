9533132903762bd12090a947cb33137eb74f5e0d
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-01-12 14:20:54 -0500

    Movie app

---
+++ Movie-fight/index.js
@@ -0,0 +1,27 @@
+const fetchData = async searchTerm => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'ead8b071',
+            s: searchTerm
+        }
+    })
+    return response.data.Search
+}
+
+const input = document. querySelector('input')
+
+const onInput = async event => {
+    const movies = await fetchData(event.target.value)
+    
+    for(let movie of movies) {
+        const div = document.createElement('div')
+
+        div.innerHTML = `
+            <img src="${movie.Poster}"/>
+            <h1>${movie.Title}</h1>
+            `
+        document.querySelector('#target').appendChild(div)
+    }
+}
+
+input.addEventListener('input', debounce(onInput, 500))
