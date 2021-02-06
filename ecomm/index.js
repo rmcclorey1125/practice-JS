390a658bc4387a71b0f4b53d997ef3cfe1dcab0b
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-02-06 16:22:12 -0500

    middleware parser

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send(`
@@ -14,8 +17,9 @@ app.get('/', (req, res) => {
     </div>
     `);
 });
-
+  
 app.post('/', (req,res) => {
+    console.log(req.body)
     res.send('Account created!!!')
 })
 
