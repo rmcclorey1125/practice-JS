f836abb09172ac064b2bdecfde0e9bc8e05309f7
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-04-02 21:58:27 -0400

    Create cart

--- ecomm/index.js
+++ ecomm/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts')
 
 const app = express();
 
@@ -11,8 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieSession({
     keys: ['fwfewfk9hg7hdqidnwjq']
 }));
-app.use(authRouter)
-app.use(productsRouter)
+app.use(authRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
+app.use(cartsRouter);
+
 
 app.listen(3000, () => {
     console.log('Listening');
