eced639b9d98a2898346d46ae24503734197249a
rmcclorey1125 <rmcclorey1@yahoo.com>
2021-04-12 22:23:29 -0400

    add mocha

--- hidash/index.js
+++ hidash/index.js
@@ -8,11 +8,21 @@ module.exports = {
             fn(arr[index], index);
         }
     },
+
     map(arr, fn) {
         const result = [];
 
         for(let i = 0; i < arr.length; i++){
             result.push(fn(arr[i], i));
+        }
+        return result;
+    },
+
+    filter(arr,fn) {
+        const result = [];
+
+        for(let i = 0; i < arr.length; i++){
+            if(fn(arr[i], i));
         }
         return result;
     }
